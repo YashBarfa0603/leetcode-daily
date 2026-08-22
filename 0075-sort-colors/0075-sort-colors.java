5406a22397e25ee84ca6a8f85601e137cd3dfe24
Yash Barfa <yashbarfa67@gmail.com>
2026-08-22 19:05:45 +0530

    Time: 3 ms (12.62%), Space: 43.6 MB (24.07%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        boolean swapped;
+        for(int i = 0; i < nums.length; i++){
+            swapped = false;
+            for (int j = 1; j< nums.length - i; j++){
+                if(nums[j] < nums[j-1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j-1];
+                    nums[j-1] = temp;
+                    swapped = true;
+                }
+            }
+        }
+    }
+}
